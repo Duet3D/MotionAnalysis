46da8ea79b983daea4d90bd7cf4eff94c3bcd1ff
Christian Hammacher <bmasterc@gmail.com>
2022-03-16 17:07:49 +0100

    Version 3.4.0-b1
    Removed lib directory
    Updated .gitignore and version number

--- lib/fft.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function transform(real: Array<number> | Float64Array, imag: Array<number> | Float64Array): void;
-export declare function inverseTransform(real: Array<number> | Float64Array, imag: Array<number> | Float64Array): void;
-export declare function transformRadix2(real: Array<number> | Float64Array, imag: Array<number> | Float64Array): void;
-export declare function transformBluestein(real: Array<number> | Float64Array, imag: Array<number> | Float64Array): void;
-export declare function convolveReal(xvec: Array<number> | Float64Array, yvec: Array<number> | Float64Array, outvec: Array<number> | Float64Array): void;
-export declare function convolveComplex(xreal: Array<number> | Float64Array, ximag: Array<number> | Float64Array, yreal: Array<number> | Float64Array, yimag: Array<number> | Float64Array, outreal: Array<number> | Float64Array, outimag: Array<number> | Float64Array): void;
-export declare function newArrayOfZeros(n: number): Array<number>;
